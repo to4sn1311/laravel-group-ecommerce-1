c18cf50aeb653dd8901c798950ece9bd90359a65
Le Minh Tung <lmtung2002@gmail.com>
2025-04-15 00:18:05 +0700

    fix create

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -7,9 +7,9 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="mb-4 bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <h2 class="text-xl font-semibold text-gray-800 dark:text-white mb-4">{{ __('Thêm Category') }}</h2>
+                    <h2 class="text-xl font-semibold text-gray-800 dark:text-white mb-4">{{ __('Thêm danh mục cấp 1:') }}</h2>
 
                     @if(session('error'))
                     <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4" role="alert">
@@ -17,17 +17,40 @@
                     </div>
                     @endif
 
+                      <!-- Thông báo lỗi -->
+                        <div id="error-messages"></div>
+                        <x-input-error :messages="$errors->get('name')" class="mt-2" />
                     <form id="category-form" method="POST" action="{{ route('categories.store') }}" class="space-y-6">
                         @csrf
-
-                        <!-- Thông báo lỗi -->
-                        <div id="error-messages"></div>
-
                         <!-- Tên -->
                         <div>
                             <x-input-label for="name" :value="__('Tên')" />
                             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
-                            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                        </div>
+                            <select name="parent_id" id="parent_id" class="hidden mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                                <option value="null">Trống</option>
+                            </select>
+                        <div class="flex items-center justify-end mt-4">
+                            <a href="{{ route('categories.index') }}" class="text-gray-500 hover:text-gray-700 font-semibold mr-4">
+                                {{ __('Hủy') }}
+                            </a>
+                            <x-primary-button>
+                                {{ __('Save') }}
+                            </x-primary-button>
+                        </div>
+                    </form>
+                </div></div>
+                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900 dark:text-gray-100">    
+                    @if($categories->isNotEmpty())
+                    <h2 class="text-xl font-semibold text-gray-800 dark:text-white mb-4">{{ __('Thêm danh mục cấp 2:') }}</h2>
+
+                    <form id="category-form" method="POST" action="{{ route('categories.store') }}" class="space-y-6">
+                        @csrf
+                        <!-- Tên -->
+                        <div>
+                            <x-input-label for="name" :value="__('Tên')" />
+                            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
                         </div>
 
                         <!-- Trường Danh Mục Cấp 1 -->
@@ -41,14 +64,16 @@
                             </select>
                         </div>
                         <div class="flex items-center justify-end mt-4">
-                            <a href="{{ route('categories.index') }}" class="text-gray-500 hover:text-gray-700 font-semibold mr-4">
-                                {{ __('Hủy') }}
-                            </a>
+                        
                             <x-primary-button>
                                 {{ __('Save') }}
                             </x-primary-button>
                         </div>
                     </form>
+
+                    @endif
+                    
+
                 </div>
             </div>
         </div>
